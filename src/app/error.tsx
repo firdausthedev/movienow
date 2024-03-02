f08fab17836a77635dc7dde33ad71fd153e9eea5
firdaus <firdausthedev@gmail.com>
2024-03-02 16:20:29 +0800

    error

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Error = () => {
+  return (
+    <div className="container h-lg p-10">
+      <p className="text-center text-white">Something went wrong.</p>
+    </div>
+  );
+};
+
+export default Error;
